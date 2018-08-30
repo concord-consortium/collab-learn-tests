b55c3aaf7fc065637b3276d494091d64a5322226
eireland <eireland@concord.org>
2018-08-30 16:02:40 -0700

    Fixed the code for getting info on each of the tabs

--- cypress/integration/learning_log_test_spec.js
+++ cypress/integration/learning_log_test_spec.js
@@ -5,7 +5,7 @@ before(function(){
 describe('Test right tabs', function(){
     it('will verify correct tab opens to correct content', function(){
         cy.get('.learning-log > .tabs > .tab').each(function(tab) {
-            cy.log('Tabs are' + this.tabs);
+            cy.log('Tab is' + tab);
             //get the tab label
             //click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
             //verify that epanded area is showing
