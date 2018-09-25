7d097477bfc834a6f023d101bcfd837d40c622a0
eireland <eireland@concord.org>
2018-09-24 20:02:23 -0700

    Moved the creating and joining group to join group test spec

--- cypress/integration/group_test_spec.js
+++ cypress/integration/group_test_spec.js
@@ -1,3 +1,47 @@
 context('Test group functionalities', function(){
+    context('test the views', function(){
+        describe('test the 1-up view', function(){
+            it('will enter text into the canvas', function(){
 
+            });
+
+        });
+        describe('test the 4-up view', function(){
+            it('will verify that 4-up view is up on button click', function(){
+
+            });
+            it('will verify that the 4-up button changes to 1-up button', function(){
+
+            });
+            it("will move horizontal splitter vertically and verify canvas size change", function () {
+
+            });
+            it('will move vertical splitter horizantally and verify canvas size change', function(){
+
+            });
+            it('will move the center handle horizontally and vertically and verify canvas size change', function (){
+
+            });
+            it('will verify editing own canvas is still possible in 4-up view', function(){
+
+            });
+            it('will verify editing is not allowed in other group members\' canvas', function(){
+
+            });
+            it('will copy text from one canvas to own canvas', function(){
+
+            });
+            it('will verify that view changes back to 1-up view',function(){
+
+            });
+
+        });
+        describe('test the 2-up view', function(){
+
+        });
+
+        describe('test the 2-up view', function(){
+
+        });
+    });
 });
