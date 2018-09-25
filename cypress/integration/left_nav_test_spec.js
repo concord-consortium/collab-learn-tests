8756bfd767cd207c7878694c9fa958d77d230c07
eireland <eireland@concord.org>
2018-09-24 19:44:30 -0700

    Checks left nav tab opens to the correct panel

--- cypress/integration/left_nav_test_spec.js
+++ cypress/integration/left_nav_test_spec.js
@@ -5,12 +5,10 @@ before(function(){
 describe('Test Left tabs',function(){
  it('will verify correct tab opens to correct content', function(){
      cy.get('.left-nav > .tabs > .tab').each(($tab,index, $list) => {
-             cy.log('Tab is' + $tab.text());
+             // cy.log('Tab is' + $tab.text());
              var title = $tab.text();//get the tab label
              cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
-             cy.get('.expanded-area > .left-nav-panel > .section >.section-header > h1').should('contain', title)//verify that expanded area is showing
-             //verify that the expanded area title corresponds to the tab label
-             //repeat for every tab.
+             cy.get('.expanded-area > .left-nav-panel > .section >.section-header > h1').should('contain', title);//verify that the expanded area title corresponds to the tab label
      })
 
  });
