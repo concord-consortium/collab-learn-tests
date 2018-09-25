f0866c9d4ca99a1a96aacc92a535a6c656e5cba8
eireland <eireland@concord.org>
2018-09-24 19:58:04 -0700

    Fixed warnings

--- cypress/integration/left_nav_test_spec.js
+++ cypress/integration/left_nav_test_spec.js
@@ -6,7 +6,7 @@ describe('Test Left tabs',function(){
  it('will verify correct tab opens to correct content', function(){
      cy.get('.left-nav > .tabs > .tab').each(($tab,index, $list) => {
              // cy.log('Tab is' + $tab.text());
-             var title = $tab.text();//get the tab label
+             let title = $tab.text();//get the tab label
              cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
              cy.get('.expanded-area > .left-nav-panel > .section >.section-header > h1').should('contain', title);//verify that the expanded area title corresponds to the tab label
      })
@@ -14,7 +14,7 @@ describe('Test Left tabs',function(){
  });
  it('will verify the transfer of the tab info to the canvas', function(){
      cy.get('.left-nav > .tabs > .tab').each(($tab, index, $list)=>{
-         var title = $tab.text();
+         let title = $tab.text();
          cy.wrap($tab).click();
          cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').should('contain', title).click();
          cy.get('.single-workspace > .workspace > .titlebar > .title').should('contain', title);
