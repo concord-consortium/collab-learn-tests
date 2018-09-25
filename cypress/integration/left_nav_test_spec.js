5ad3ba77e7e9b3d3ab544f6c55fc999556d6ad69
eireland <eireland@concord.org>
2018-09-24 19:43:15 -0700

    Checks left nav tab opens to the correct panel

--- cypress/integration/left_nav_test_spec.js
+++ cypress/integration/left_nav_test_spec.js
@@ -1,17 +1,17 @@
 before(function(){
-    cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=dev');
+    cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=5&demoUser=student:1&demoOffering=4&problem=1.1');
 });
 
 describe('Test Left tabs',function(){
  it('will verify correct tab opens to correct content', function(){
-     cy.get('.left-nav > .tabs > .tab').each(function(tab){
-             cy.log('Tab is' + tab);
-             //get the tab label
-             //click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
-             //verify that expanded area is showing
+     cy.get('.left-nav > .tabs > .tab').each(($tab,index, $list) => {
+             cy.log('Tab is' + $tab.text());
+             var title = $tab.text();//get the tab label
+             cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
+             cy.get('.expanded-area > .left-nav-panel > .section >.section-header > h1').should('contain', title)//verify that expanded area is showing
              //verify that the expanded area title corresponds to the tab label
              //repeat for every tab.
-     });
+     })
 
  });
  it('will verify the transfer of the tab info to the canvas', function(){
