2d6b65c0e6a66b9b0eb61f18e8351a2f6f34d3ec
eireland <eireland@concord.org>
2018-09-26 13:52:08 -0700

    Added test for switching from 1-up to 4-up views Added test for sharing and unsharing button

--- cypress/integration/canvas_test_spec.js
+++ cypress/integration/canvas_test_spec.js
@@ -1,45 +1,65 @@
+
 before(function(){
     cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=2&demoUser=student:1&demoOffering=4&problem=1.1');
-
     cy.get('#leftNavTab0').click();
     cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click();
-    // cy.get('.left-nav > .tabs > .tab').each(($tab, index, $list)=>{
-    //     let title = $tab.text();
-    //     cy.wrap($tab).click();
-    //     cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').should('contain', title).click();
-    // });
 });
 
 context('Test Canvas', function(){
-    // This should test the 1-up and the 4-up views)
-    context('test the overall canvas', function(){
+
+    context('test canvas tools', function(){
+
        describe('test header elements', function(){
-           // it('verifies header title appears correctly', function(){
-           //      cy.get('.workspace > .titlebar > .title').should('contain','Workspace Name');
-           // });
-           it('verifies views button changes the current workspace view to the corresponding one', function(){
-               // cy.get('.workspace > .titlebar > .actions > span').should('contain','4-up').click();
-               // cy.get('.canvas-area > .canvas').should('be.visible');
-               // // cy.get('.workspace > .titlebar > .actions > span').click();
-               // cy.get('.workspace > .titlebar > .actions > span').should('contain','1-up').click();
-               // cy.get('.canvas-area > .four-up').should('be.visible');
-               // // cy.get('.workspace > .titlebar > .actions > span').click();
-               // cy.get('.workspace > .titlebar > .actions > span').should('contain','4-up');
-               // cy.get('.canvas-area > .canvas').should('be.visible');
+           it('verifies header title appears correctly', function(){
+                cy.get('.workspace > .titlebar > .title').should('contain','Introduction');
            });
 
+           it('verifies views button changes when clicked and shows the correct corresponding workspace view', function(){
+               //1-up view has 4-up button visible and 1-up canvas
+               cy.get('.workspace > .titlebar > .actions > .icon-up1').should('be.visible');
+               cy.get('.canvas-area > .canvas').should('be.visible');
+               cy.get('.canvas-container.north-east').should('not.be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-up1').click();
+               //4-up view is visible and 1-up button is visible
+               cy.get('.workspace > .titlebar > .actions > .icon-up').should('be.visible');
+               cy.get('.canvas-area > .four-up >.canvas-container.north-east').should('be.visible');
+               cy.get('.canvas-area > .four-up >.canvas-container.north-west').should('be.visible');
+               cy.get('.canvas-area > .four-up >.canvas-container.south-east').should('be.visible');
+               cy.get('.canvas-area > .four-up >.canvas-container.south-west').should('be.visible');
+               cy.get('.canvas-area > .canvas').should('not.be.visible');
+               //can get back to 1 up view from 4 up
+               cy.get('.workspace > .titlebar > .actions > .icon-up').click();
+               cy.get('.canvas-area > .canvas').should('be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-up1').should('be.visible');
+
+               cy.get('.canvas-container.north-east').should('not.be.visible');
+           });
+
+           it('verify share button', function(){
+               cy.get('.workspace > .titlebar > .actions > .icon-share').should('be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-unshare').should('not.be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-share').click();
+               cy.get('.workspace > .titlebar > .actions > .icon-share').should('not.be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-unshare').should('be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-unshare').click();
+               cy.get('.workspace > .titlebar > .actions > .icon-share').should('be.visible');
+               cy.get('.workspace > .titlebar > .actions > .icon-unshare').should('not.be.visible');
+           });
+       }) ;
+
+       describe('test footer elements', function(){
            it('verify supports comes up correctly', function(){
 
            });
-           it('verify share button', function(){
-               
-           });
-       }) ;
+
+            it('verify 2 up button, and correct corresponding view comes up', function(){
+
+            });
+       });
     });
 
-
+    context('test the tool palette', function(){
     //This should test the tools in the tool shelf
-    context('test the canvas tool shelf', function(){
         describe('test the selection tool', function(){
             it('verify the selection tool becomes active when clicked', function() {
                 cy.get('.single-workspace > .workspace > .toolbar > .tool.select').click()
@@ -67,28 +87,20 @@ context('Test Canvas', function(){
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click();
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click(40,35, {force:true});
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > .JXGtext').last().should('contain', 'A' );
-                // cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > svg > g > ellipse').last().trigger('mouseover',{force:true});
-                // cy.wait(2000);
-                // cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > JXGinforbox').first().should('be.visible')
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click(140,70, {force:true});
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > .JXGtext').last().should('contain', 'B' );
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click(260,50, {force:true});
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > .JXGtext').last().should('contain', 'C' );
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click();
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > .JXGtext').last().should('contain', 'D' );
-                // cy.get('.canvas-area > .canvas > .document-content > .tool-tile.selected').should('have.class','selected');
-                // cy.get('.single-workspace >.workspace > .toolbar > .tool.delete').click({force:true});
-
             });
+        });
+    });
 
+    context('save and restore of canvas', function(){
         describe('verify that canvas is saved', function(){
             it('will close and reopen the canvas and verify it looks the same', function() {
-                //open the my work tab
-                //click a different canvas
-                //verify canvas is shown
-                //open the my work tab
-                //click the introduction canvas
-                //verify intro canvas is showing
+                //open the my work tab, click a different canvas, verify canvas is shown, open the my work tab, click the introduction canvas, verify intro canvas is showing
                 cy.get('#rightNavTabMy\\ Work').click({force:true});
                 cy.get('.list > .list-item[title*="Initial"]').click();
                 cy.get('.single-workspace > .workspace > .titlebar > .title').should('contain', 'Initial');
@@ -102,15 +114,28 @@ context('Test Canvas', function(){
                 cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool > .JXGtext').each(($point, index, $list)=>{}).then(($list)=>{
                     expect($list).to.have.length(4);
                 });
-                //Delete elements in the canvas
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').last().focus().click();
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile.selected').should('have.class','selected');
-                cy.get('.single-workspace >.workspace > .toolbar > .tool.delete').click({force:true});
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click();
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile.selected').should('have.class','selected');
-                cy.get('.single-workspace >.workspace > .toolbar > .tool.delete').click({force:true});
             });
         });
+
+        describe('verify that if user opens same canvas from on left-nav tab, saved canvas opens', function() {
+
+        });
+
+        describe('verify that if user leaves a canvas in four-four up view, restore is also in four up view', function(){
+
+        });
+    });
+
+
+    context('delete elements from canvas', function(){
+        it('will delete elements from canvas', function(){
+            // //Delete elements in the canvas
+            cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').last().focus().click();
+            cy.get('.canvas-area > .canvas > .document-content > .tool-tile.selected').should('have.class','selected');
+            cy.get('.single-workspace >.workspace > .toolbar > .tool.delete').click({force:true});
+            cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .geometry-tool').last().click();
+            cy.get('.canvas-area > .canvas > .document-content > .tool-tile.selected').should('have.class','selected');
+            cy.get('.single-workspace >.workspace > .toolbar > .tool.delete').click({force:true});
         });
     });
 
