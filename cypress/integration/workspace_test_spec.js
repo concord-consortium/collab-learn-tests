c5359c4581b93f7aa3274f5d7861248cce8ffd23
eireland <eireland@concord.org>
2018-09-27 09:47:41 -0700

    Added taking screenshots of loaded problems and tabs. Move sequence of tests. Added some cleaning up at the end of each it so next it starts with a clean slate.

--- cypress/integration/workspace_test_spec.js
+++ cypress/integration/workspace_test_spec.js
@@ -1,7 +1,7 @@
 context('Test the overall workspace', function(){
 
     describe('Test loading problem pages', function(){
-        it('will open the specified problem', function(){
+        it('should load specified problems and tabs successfully', function(){
             const problems = [
                 {
                     number:'1.1',
@@ -52,30 +52,19 @@ context('Test the overall workspace', function(){
             problems.forEach(function(problem){
                 cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=5&demoUser=student:9&demoOffering=4&problem='+problem.number);
                 // cy.visit('http://localhost:8080/?appMode=demo&demoClass=5&demoUser=student:1&demoOffering=1&problem='+problem.number);
-                it('verify Problem title presence and correctness', function(problem){
-                    cy.get('.problem').should('contain', problem.title+' '+problem.subtitle);
-                });
+                    cy.get('.problem').should('contain', problem.title+': '+problem.subtitle);
+                    cy.screenshot(problem.title);
+                    cy.get('.left-nav > .tabs > .tab').each(($tab,index, $list) => {
+                        let tabTitle = $tab.text();//get the tab label
+                        let probTitle = problem.title;
+                        cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
+                        cy.screenshot(probTitle + ' ' + tabTitle);
+                })
             });
         });
     });
 
     describe('Desktop functionalities', function(){
-        it('will verify that left nav area is closes when other tabs are opened', function(){
-            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=5&demoUser=student:9&demoOffering=4&problem=1.1');
-            cy.get('.left-nav > .tabs > .tab:first').click(); //left nav expand area should be visible
-            cy.get('.left-nav.expanded').should('be.visible');
-            cy.get('.right-nav>.tabs.expanded').should('not.be.visible');
-            cy.get('.bottom-nav.expanded').should('not.be.visible');
-            cy.get('#rightNavTabMy\\ Work').click(); //my work expand area should be visible
-            cy.get('.left-nav.expanded').should('not.be.visible');
-            cy.get('.right-nav>.tabs.expanded').should('be.visible');
-            cy.get('.bottom-nav.expanded').should('not.be.visible');
-            cy.get('#learningLogTab').click(); //learning log expand area should be visible
-            cy.get('.left-nav.expanded').should('not.be.visible');
-            cy.get('.right-nav>.tabs.expanded').should('be.visible');
-            cy.get('.bottom-nav.expanded').should('be.visible');
-        });
-
         it('will verify that clicking on tab closes the nav area', function(){
             cy.get('.left-nav > .tabs > .tab:first').click(); //left nav expand area should be visible
             cy.get('.left-nav.expanded').should('be.visible');
@@ -91,6 +80,25 @@ context('Test the overall workspace', function(){
             cy.get('.bottom-nav.expanded').should('be.visible');
             cy.get('#learningLogTab').click(); //learning log expand area should not be visible
             cy.get('.bottom-nav.expanded').should('not.be.visible');
+        });
+
+        it('will verify that left nav area is closes when other tabs are opened', function(){
+            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=5&demoUser=student:9&demoOffering=4&problem=1.1');
+            cy.get('.left-nav > .tabs > .tab:first').click(); //left nav expand area should be visible
+            cy.get('.left-nav.expanded').should('be.visible');
+            cy.get('.right-nav>.tabs.expanded').should('not.be.visible');
+            cy.get('.bottom-nav.expanded').should('not.be.visible');
+            cy.get('#rightNavTabMy\\ Work').click(); //my work expand area should be visible
+            cy.get('.left-nav.expanded').should('not.be.visible');
+            cy.get('.right-nav>.tabs.expanded').should('be.visible');
+            cy.get('.bottom-nav.expanded').should('not.be.visible');
+            cy.get('#learningLogTab').click(); //learning log expand area should be visible
+            cy.get('.left-nav.expanded').should('not.be.visible');
+            cy.get('.right-nav>.tabs.expanded').should('be.visible');
+            cy.get('.bottom-nav.expanded').should('be.visible');
+            //close all tabs to clear workspace for next test
+            cy.get('#learningLogTab').click(); //learning log expand area should be visible
+            cy.get('#rightNavTabMy\\ Work').click(); //my work expand area should be visible
         });
 
         it('will verify that My Work tab is still visible and clickable when Learning Log is expanded', function(){
