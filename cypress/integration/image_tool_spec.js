65f5a940972d86022e4fdd742a669b21bf0d0cd1
eireland <eireland@concord.org>
2018-09-28 16:45:40 -0700

    Added script to drag image from one tab to a different canvas

--- cypress/integration/image_tool_spec.js
+++ cypress/integration/image_tool_spec.js
@@ -17,5 +17,13 @@ context('Test image functionalities', function(){
             cy.get('.left-nav.expanded > div.expanded-area.expanded > .left-nav-panel > .section > .canvas > .document-content > .tool-tile > .image-tool > img').trigger('mousedown').trigger('mousemove',{pageX: 660 }, {pageY: 475}).trigger('mouseup',{force:true});
             cy.get('#leftNavTab0').click(); //close tab
         });
+        it('verify image can be dragged on to different titled canvas', function(){
+            cy.get('#leftNavTab1').click();
+            cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click();
+            cy.get('.workspace > .titlebar > .title').should('contain','Initial');
+            cy.get('#leftNavTab0').click();
+            cy.get('.left-nav.expanded > div.expanded-area.expanded > .left-nav-panel > .section > .canvas > .document-content > .tool-tile > .image-tool > img').trigger('mousedown').trigger('mousemove',{pageX: 660 }, {pageY: 475}).trigger('mouseup',{force:true});
+            cy.get('#leftNavTab0').click(); //close tab
+        });
     });
 });
