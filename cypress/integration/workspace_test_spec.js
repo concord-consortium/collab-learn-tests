46c05f8fd269cc114629d0e550e8661d9de2f8df
eireland <eireland@concord.org>
2018-10-03 12:58:43 -0700

    Changed test to make it a general right navs can be opened when learning log is open Fixed URL to point to appMode=qa

--- cypress/integration/workspace_test_spec.js
+++ cypress/integration/workspace_test_spec.js
@@ -1,4 +1,7 @@
 context('Test the overall workspace', function(){
+    before(function(){
+        cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=5&fakeUser=student:1&fakeOffering=1&qaGroup=1&problem=1.1');
+    });
 
     describe('Test loading problem pages', function(){
         it('should load specified problems and tabs successfully', function(){
@@ -50,7 +53,7 @@ context('Test the overall workspace', function(){
                 }
             ];
             problems.forEach(function(problem){
-                cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=5&demoUser=student:9&demoOffering=4&problem='+problem.number);
+                cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=5&fakeUser=student:9&fakeOffering=4&qaGroup=1&problem='+problem.number);
                 // cy.visit('http://localhost:8080/?appMode=demo&demoClass=5&demoUser=student:1&demoOffering=1&problem='+problem.number);
                     cy.get('.problem').should('contain', problem.title+': '+problem.subtitle);
                     cy.screenshot(problem.title);
@@ -66,9 +69,9 @@ context('Test the overall workspace', function(){
 
     describe('Desktop functionalities', function(){
         it('will verify that clicking on tab closes the nav area', function(){
-            cy.get('.left-nav > .tabs > .tab:first').click(); //left nav expand area should be visible
+            cy.get('.left-nav > .tabs > .tab:first').click({force:true}); //left nav expand area should be visible
             cy.get('.left-nav.expanded').should('be.visible');
-            cy.get('.left-nav > .tabs > .tab:first').click(); //left nav expand area should not be visible
+            cy.get('.left-nav > .tabs > .tab:first').click({force:true}); //left nav expand area should not be visible
             cy.get('.left-nav.expanded').should('not.be.visible');
 
             cy.get('#rightNavTabMy\\ Work').click(); //my work expand area should be visible
@@ -82,8 +85,8 @@ context('Test the overall workspace', function(){
             cy.get('.bottom-nav.expanded').should('not.be.visible');
         });
 
-        it('will verify that left nav area is closes when other tabs are opened', function(){
-            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=demo&demoClass=5&demoUser=student:9&demoOffering=4&problem=1.1');
+        it('will verify that left nav area is closes when other tabs are opened', function(){ //should this be tab closes when no longer in that area? my work and left nav
+            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=5&fakeUser=student:1&fakeOffering=1&qaGroup=1&problem=1.1');
             cy.get('.left-nav > .tabs > .tab:first').click(); //left nav expand area should be visible
             cy.get('.left-nav.expanded').should('be.visible');
             cy.get('.right-nav>.tabs.expanded').should('not.be.visible');
@@ -101,12 +104,14 @@ context('Test the overall workspace', function(){
             cy.get('#rightNavTabMy\\ Work').click(); //my work expand area should be visible
         });
 
-        it('will verify that My Work tab is still visible and clickable when Learning Log is expanded', function(){
+        it.only('will verify that right nav tabs are still visible and clickable when Learning Log is expanded', function(){
             cy.get('#learningLogTab').click(); //learning log expand area should be visible
             cy.get('.bottom-nav.expanded').should('be.visible');
-            cy.get('#rightNavTabMy\\ Work').should('be.visible');
-            cy.get('#rightNavTabMy\\ Work').click();
-            cy.get('.right-nav>.tabs.expanded').should('be.visible');
+            cy.get('.right-nav > .tabs > .tab').each(($tab,index, $list) => {
+                cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
+                cy.get('.right-nav>.tabs.expanded').should('be.visible');
+            })
+
         });
 
     });
