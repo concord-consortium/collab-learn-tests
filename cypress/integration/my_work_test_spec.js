224bae8a67d2a4d56dca0ccba24e80c489f502f7
eireland <eireland@concord.org>
2018-10-04 14:48:28 -0700

    Changed function for opening left nav canvas Changed selectors for My Work tab

--- cypress/integration/my_work_test_spec.js
+++ cypress/integration/my_work_test_spec.js
@@ -1,23 +1,22 @@
 describe('Test right nav tabs', function(){
     function openLeftNavCanvases(){
-        cy.get('.left-nav > .tabs > .tab').each(($tab, index, $list)=> {
-            let title = $tab.text();
-            cy.wrap($tab).click({force:true});
-            cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').should('contain', title).click();
-            cy.get('.single-workspace > .workspace > .titlebar > .title').should('contain', title);
-        })
+        cy.get('.left-nav > .tabs > .tab').each(($tab,index,$tabList)=>{})
+            .then(($tabList)=>{
+                var i=0;
+                for (i=0;i<$tabList.length;i++){
+                    cy.get('#leftNavTab'+i).click({force:true})
+                    cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click();
+                }
+            });
     }
 
     //This assumes there were canvases previously created from the left nav tabs
-    it('will verify that opened content is listed in My Work tab space', function(){ //still need to verify the titles match the titles from opened canvases
+    it('verify that opened content is listed in My Work tab space', function(){ //still need to verify the titles match the titles from opened canvases
         openLeftNavCanvases();
-        cy.get('.right-nav > .tabs > .tab').each(($tab, index, $list)=>{
-            cy.log('Tab is' + $tab.text());
-            cy.wrap($tab).click();
-            cy.get('.expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
+        cy.get('#rightNavTabMy\\ Work.tab').click();
+        cy.get('.expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
                 cy.log('Title is ' + $item.text());
-            })
-        });
+            });
     });
     it('will open the correct canvas selected from the My Work list', function(){
             cy.get('.expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
@@ -28,7 +27,7 @@ describe('Test right nav tabs', function(){
                         let canvasTitle=$canvasTitle.text();
                         expect($canvasTitle.text()).to.contain(title);
                 });
-                cy.get('.right-nav > .tabs > .tab').click();
+                cy.get('#rightNavTabMy\\ Work.tab').click();
             });
     });
     it('will open correct canvas from Class Work list', function(){ //this assumes there are published work
