237d9a9cff7afc4ec0ead7a989afdc7343133461
eireland <eireland@concord.org>
2018-10-05 12:19:14 -0700

    Fixed script so that it's more reliable when cycling through the tabs

--- cypress/integration/left_nav_test_spec.js
+++ cypress/integration/left_nav_test_spec.js
@@ -1,13 +1,24 @@
 describe('Test Left tabs',function(){
  it('will verify the transfer of the tab info to the canvas', function(){
-     cy.get('.left-nav > .tabs > .tab').each(($tab, index, $list)=>{
-         let title = $tab.text();
-         cy.wrap($tab).click({force:true});
-         cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').should('contain', title).click();
-         cy.get('.single-workspace > .workspace > .titlebar > .title').should('contain', title);
+    let titleArr = [], i=0;
+
+     cy.get('.left-nav > .tabs > .tab').each(($tab, index, $tabList)=>{
+         titleArr.push($tab.text());
+     }).then(($tab)=> {
+
+         for (i = 0; i < $tab.length - 1; i++) {
+             let title = $tab.text;
+             cy.get('#leftNavTab' + i).click({force:true});
+                 cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').should('contain', titleArr[i]).click({force: true});
+                     cy.get('.single-workspace > .document > .titlebar > .title').should('contain', titleArr[i]);
+         }
      })
 
+     // cy.get('.left-nav > .tabs > .tab').each(($tab, index, $tabList)=>{
+     //     let title = $tab.text();
+     //     cy.wrap($tab).click({force:true});
+     //     cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').should('contain', title).click({force:true});
+     //     cy.get('.single-workspace > .document > .titlebar > .title').should('contain', title);
+     // })
  });
-
-
 });
