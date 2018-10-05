9436098a69fe3262af0cb46830d11803d4755340
eireland <eireland@concord.org>
2018-10-05 15:42:31 -0700

    Added test for restoring published canvases from Class Work

--- cypress/integration/my_work_test_spec.js
+++ cypress/integration/my_work_test_spec.js
@@ -4,34 +4,64 @@ describe('Test right nav tabs', function(){
             .then(($tabList)=>{
                 var i=0;
                 for (i=0;i<$tabList.length;i++){
-                    cy.get('#leftNavTab'+i).click({force:true})
-                    cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click();
+                    cy.get('#leftNavTab'+i).click({force:true});
+                    cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click({force:true});
                 }
+            });
+    }
+    function publishCanvases(){
+        cy.get('.left-nav > .tabs > .tab').each(($tab,index,$tabList)=>{})
+            .then(($tabList)=>{
+                var i=0;
+                for (i=0;i<$tabList.length;i++){
+                    cy.get('#leftNavTab'+i).click({force:true});
+                    cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click({force:true});
+                    cy.get('.document > .titlebar > .actions > .icon-publish').click();
+                    cy.get('.dialog > .dialog-container > .dialog-title').should('contain', 'Published');
+                    cy.get('.dialog > .dialog-container > .dialog-contents > .dialog-buttons > #okButton').click();
+                    cy.get('.dialog > .dialog-container > .dialog-title').should('not.exist');
+                    cy.get('.document > .titlebar > .actions > .icon-publish').should('exist');                }
             });
     }
 
     //This assumes there were canvases previously created from the left nav tabs
     it('verify that opened content is listed in My Work tab space', function(){ //still need to verify the titles match the titles from opened canvases
         openLeftNavCanvases();
-        cy.get('#rightNavTabMy\\ Work.tab').click();
-        cy.get('.expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
+        cy.wait(3000);
+        cy.get('#rightNavTabMy\\ Work.tab').click({force:true});
+        cy.get('.right-nav > .expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
                 cy.log('Title is ' + $item.text());
             });
     });
     it('will open the correct canvas selected from the My Work list', function(){
-            cy.get('.expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
+        cy.get('#rightNavTabMy\\ Work.tab').click({force:true});
+        cy.get('#rightNavTabMy\\ Work.tab').click({force:true});
+        cy.get('.right-nav > .expanded-area.expanded > .contents > .my-work > .list > .list-item').each(($item,index,$list)=>{
                 let title= $item.text().replace(/[^\x00-\x7F]/g, "");
                 cy.wrap($item).click();
-                cy.get('.single-workspace > .workspace > .titlebar > .title')
+                cy.get('.single-workspace > .document > .titlebar > .title')
                     .then(($canvasTitle)=>{
                         let canvasTitle=$canvasTitle.text();
                         expect($canvasTitle.text()).to.contain(title);
                 });
-                cy.get('#rightNavTabMy\\ Work.tab').click();
-            });
+                cy.get('#rightNavTabMy\\ Work.tab').click({force:true});
+                cy.wait(1000);
+        });
     });
-    it('will open correct canvas from Class Work list', function(){ //this assumes there are published work
-        cy.log('need to write this test');
-        expect(4).to.equal(3);
+    it.only('will open correct canvas from Class Work list', function(){ //this assumes there are published work
+        publishCanvases();
+        cy.get('#rightNavTabClass\\ Work.tab').click({force:true});
+        // cy.get('#rightNavTabClass\\ Work.tab').click({force:true});
+        cy.get('.right-nav > .expanded-area.expanded > .contents > .class-work > .list > .list-item').each(($item,index,$list)=>{
+            let title= $item.text().replace(/[^\x00-\x7F]/g, "").split('Group');
+            cy.wrap($item).click();
+            cy.get('.single-workspace > .document > .titlebar > .title')
+                .then(($canvasTitle)=>{
+                    let canvasTitle=$canvasTitle.text();
+                    expect($canvasTitle.text()).to.contain(title[0]);
+                });
+            cy.get('#rightNavTabClass\\ Work.tab').click({force:true});
+            cy.wait(1000);
+        });
     })
 });
