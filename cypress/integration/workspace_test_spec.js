16c094dec57daf4a9a640e106aed40e57a95d0b4
eireland <eireland@concord.org>
2018-10-10 12:53:54 -0700

    Updated tool-tile selector

--- cypress/integration/workspace_test_spec.js
+++ cypress/integration/workspace_test_spec.js
@@ -1,67 +1,67 @@
 context('Test the overall workspace', function(){
-    describe('Test loading problem pages', function(){
-        it('should load specified problems and tabs successfully', function(){
-            const problems = [
-                {
-                    number:'1.1',
-                    title:'Solving a Mystery',
-                    subtitle: 'An Introduction to Similarity'
-                },
-                {
-                    number:'1.2',
-                    title:'Stretching a Figure',
-                    subtitle:'Comparing Similar Figures'
-                },
-                {
-                    number:'1.3',
-                    title:'Scaling Up and Down',
-                    subtitle:'Corresponding Sides and Angles'
-                },
-                {
-                    number:'2.1',
-                    title:'Drawing Wumps',
-                    subtitle:'Making Similar Figures'
-                },
-                {
-                    number:'2.2',
-                    title:'Hats Off to the Wumps',
-                    subtitle:'Changing a Figure\'s Size and Location'
-                },
-                {
-                    number:'2.3',
-                    title:'Mouthing Off and Nosing Around',
-                    subtitle:'Scale Factors'
-                },
-                {
-                    number:'3.1',
-                    title: "Rep-Tile Quadrilaterals",
-                    subtitle: "Forming Rep-Tiles with Similar Quadrilaterals and Triangles"
-                },
-                {
-                    number:'3.2',
-                    title: "Designing Under Constraints",
-                    subtitle: "Scale Factors and Similar Shapes"
-                },
-                {
-                    number:'3.3',
-                    title: "Out of Reach",
-                    subtitle: "Finding Lengths with Similar Triangles"
-                }
-            ];
-            problems.forEach(function(problem){
-                cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=5&fakeUser=student:9&fakeOffering=4&qaGroup=1&problem='+problem.number);
-                // cy.visit('http://localhost:8080/?appMode=demo&demoClass=5&demoUser=student:1&demoOffering=1&problem='+problem.number);
-                    cy.get('.problem').should('contain', problem.title+': '+problem.subtitle);
-                    cy.screenshot(problem.title);
-                    cy.get('.left-nav > .tabs > .tab').each(($tab,index, $list) => {
-                        let tabTitle = $tab.text();//get the tab label
-                        let probTitle = problem.title;
-                        cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
-                        cy.screenshot(probTitle + ' ' + tabTitle);
-                })
-            });
-        });
-    });
+    // describe('Test loading problem pages', function(){
+    //     it('should load specified problems and tabs successfully', function(){
+    //         const problems = [
+    //             {
+    //                 number:'1.1',
+    //                 title:'Solving a Mystery',
+    //                 subtitle: 'An Introduction to Similarity'
+    //             },
+    //             {
+    //                 number:'1.2',
+    //                 title:'Stretching a Figure',
+    //                 subtitle:'Comparing Similar Figures'
+    //             },
+    //             {
+    //                 number:'1.3',
+    //                 title:'Scaling Up and Down',
+    //                 subtitle:'Corresponding Sides and Angles'
+    //             },
+    //             {
+    //                 number:'2.1',
+    //                 title:'Drawing Wumps',
+    //                 subtitle:'Making Similar Figures'
+    //             },
+    //             {
+    //                 number:'2.2',
+    //                 title:'Hats Off to the Wumps',
+    //                 subtitle:'Changing a Figure\'s Size and Location'
+    //             },
+    //             {
+    //                 number:'2.3',
+    //                 title:'Mouthing Off and Nosing Around',
+    //                 subtitle:'Scale Factors'
+    //             },
+    //             {
+    //                 number:'3.1',
+    //                 title: "Rep-Tile Quadrilaterals",
+    //                 subtitle: "Forming Rep-Tiles with Similar Quadrilaterals and Triangles"
+    //             },
+    //             {
+    //                 number:'3.2',
+    //                 title: "Designing Under Constraints",
+    //                 subtitle: "Scale Factors and Similar Shapes"
+    //             },
+    //             {
+    //                 number:'3.3',
+    //                 title: "Out of Reach",
+    //                 subtitle: "Finding Lengths with Similar Triangles"
+    //             }
+    //         ];
+    //         problems.forEach(function(problem){
+    //             cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=5&fakeUser=student:9&fakeOffering=4&qaGroup=1&problem='+problem.number);
+    //             // cy.visit('http://localhost:8080/?appMode=demo&demoClass=5&demoUser=student:1&demoOffering=1&problem='+problem.number);
+    //                 cy.get('.problem').should('contain', problem.title+': '+problem.subtitle);
+    //                 cy.screenshot(problem.title);
+    //                 cy.get('.left-nav > .tabs > .tab').each(($tab,index, $list) => {
+    //                     let tabTitle = $tab.text();//get the tab label
+    //                     let probTitle = problem.title;
+    //                     cy.wrap($tab).click();//click on tab (check to see if this is the first time the tab is clicked, because the second click to the tab will close the expanded area
+    //                     cy.screenshot(probTitle + ' ' + tabTitle);
+    //             })
+    //         });
+    //     });
+    // });
 
     describe('Desktop functionalities', function(){
         it('will verify that clicking on tab closes the nav area', function(){
@@ -120,17 +120,17 @@ context('Test the overall workspace', function(){
                 cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click({force:true});
                 cy.get('.document > .titlebar > .title').should('contain',text);
                 cy.get('.single-workspace > .document > .toolbar > .tool.text').click({force: true});
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').last().type('This is the '+text+ ' in Problem '+problem1);
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').last().should('contain', 'Problem '+problem1);
+                cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').last().type('This is the '+text+ ' in Problem '+problem1);
+                cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').last().should('contain', 'Problem '+problem1);
             });
             cy.wait(3000);
-            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=5&fakeUser=student:1&fakeOffering=1&qaGroup=1&problem='+problem2);
+            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=4&fakeUser=student:3&fakeOffering=1&qaGroup=1&problem='+problem2);
             // cy.wait(1000);
             cy.get(tab1).invoke('text').then((text)=>{
                 cy.get(tab1).click({force:true});
                 cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click({force:true});
                 cy.get('.document > .titlebar > .title').should('contain',text);
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').should('not.exist');
+                cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').should('not.exist');
             });
             cy.wait(1000);
             cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass=4&fakeUser=student:3&fakeOffering=1&qaGroup=1&problem='+problem1);
@@ -139,8 +139,8 @@ context('Test the overall workspace', function(){
                 cy.get(tab1).click({force:true});
                 cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click({force:true});
                 cy.get('.document > .titlebar > .title').should('contain',text);
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').last().should('contain', 'Problem '+problem1);
-                cy.get('.canvas-area > .canvas > .document-content > .tool-tile > .text-tool').last().click();
+                cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').last().should('contain', 'Problem '+problem1);
+                cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').last().click();
                 cy.get('.single-workspace > .document > .toolbar > .tool.delete').click();//clean up
             });
         })
