374b6506b0a3972322ad05e5a1598a2fe91db7a5
eireland <eireland@concord.org>
2018-10-11 16:46:09 -0700

    Fixed switch for openLeftNavTab

--- cypress/integration/elements/LeftNav.js
+++ cypress/integration/elements/LeftNav.js
@@ -2,9 +2,31 @@ class LeftNav{
     getLeftNavTabs(){
         return cy.get('.left-nav > .tabs > .tab');
     }
+
+    openLeftNavTab(title){ //Not the best way. Need a better implementation
+        switch(title){
+            case 'Introduction':
+                cy.get('#leftNavTab0').click({force:true});
+                break;
+            case 'Initial Challenge':
+                cy.get('#leftNavTab1').click({force:true});
+                break;
+            case 'What if':
+                cy.get('#leftNavTab2').click({force:true});
+                break;
+            case 'Now What':
+                cy.get('#leftNavTab3').click({force:true});
+                break;
+            case 'Extra':
+                cy.get('#leftNavTab4').click({force:true});
+                break;
+        }
+    }
+
     getOpenToWorkspaceButton(){
         return cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button');
     }
+
     openToWorkspace() {
         this.getOpenToWorkspaceButton().click({force:true});
     }
