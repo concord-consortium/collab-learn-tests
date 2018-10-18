8db78431d97e17ea2aef95da9136195addd7e82b
eireland <eireland@concord.org>
2018-10-19 05:50:40 +0800

    Initial tests for Teacher Dashboard

--- cypress/integration/teacher_test_spec.js
+++ cypress/integration/teacher_test_spec.js
@@ -1,4 +1,9 @@
-import Header from './elements/Header.js'
+import Header from './elements/Header.js';
+import RightNav from './elements/RightNav';
+import LeftNav from './elements/LeftNav';
+import Canvas from './elements/Canvas';
+import Workspace from './elements/Workspace';
+import TeacherDashboard from './elements/TeacherDashboard';
 
 let qaClass = 10,
     qaOffering = 10,
@@ -7,64 +12,70 @@ let qaClass = 10,
     studentArr=[15,16,17,18];
 let teacher = 10;
 
-function setupGroup(){
+let header = new Header,
+    rightNav = new RightNav,
+    leftNav = new LeftNav,
+    canvas = new Canvas,
+    workspace = new Workspace,
+    teacherDashboard = new TeacherDashboard;
+
+
+function setupGroup(group){
     let i=0;
 
-    for (i=0;i<studentArr.length;i++) {
-        cy.wait(5000);
-        cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&qaGroup='+qaGroup+'&fakeClass='+qaClass+'&fakeUser=student:'+studentArr[i]+'&fakeOffering='+qaOffering+'&problem='+problem);
-        cy.get('#leftNavTab3').click();
-        cy.get('.left-nav-panel > .section > .canvas > .document-content > .buttons > button').click();
-        cy.get('.single-workspace > .document > .toolbar > .tool.text').click({force: true});
-        cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').last().type('This is to test the 4-up view of S'+studentArr[i]);
-        cy.get('.canvas-area > .canvas > .document-content > .tile-row > .tool-tile > .text-tool').last().should('contain', '4-up').and('contain','S'+studentArr[i]);
-        cy.get('.single-workspace > .document > .titlebar > .actions > .icon-share').click();//all students will share their canvas
+    for (i=0;i<group.length;i++) {
+        cy.wait(2000);
+        cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&qaGroup='+qaGroup+'&fakeClass='+qaClass+'&fakeUser=student:'+group[i]+'&fakeOffering='+qaOffering+'&problem='+problem);
+        leftNav.openLeftNavTab('Now What');
+        leftNav.openToWorkspace();
+        canvas.addTextTile();
+        canvas.enterText('This is to test the 4-up view of S'+group[i]);
+        canvas.getTextTile().last().should('contain', '4-up').and('contain','S'+group[i]);
+        canvas.shareCanvas();//all students will share their canvas
         cy.wait(1000);
     }
     //verify Group num and there are 4 students in the group
-    cy.get('.app > .group-view > .header > .group > .name').should('contain','Group '+qaGroup);
-    cy.get('.app > .group-view > .header > .group > .members > .member').each(($member,index, $list)=>{
-        expect(['S15','S16','S17','S18']).to.include($member.text());
+    header.getGroupName().should('contain','Group '+qaGroup);
+    header.getGroupMembers().each(($member,index, $list)=>{
+        expect(['S'+group[0],'S'+group[1],'S'+group[2],'S'+group[3]]).to.include($member.text());
     });
 }
 
 context('Teacher workspace',function(){ //does not have My Work tab and has Teacher in user name
     describe('Check header area for correctness', function(){
 
-        const workspace = new Header();
-        it('set up group and will go to a teacher view of the site', function(){
-            setupGroup();
-            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass='+classroom+'&fakeUser=teacher:'+teacher+'&fakeOffering='+offering+'&problem='+problem+'&qaGroup='+qaGroup);
+        it('set up group and will go to a teacher view of the site', function() {
+            setupGroup(studentArr);
+            cy.visit('https://collaborative-learning.concord.org/branch/master/?appMode=qa&fakeClass='+qaClass+'&fakeUser=teacher:'+teacher+'&fakeOffering='+qaOffering+'&problem='+problem+'&qaGroup='+qaGroup);
         });
+
         it('will verify if class name is correct', function(){
-            workspace.getClassName().should('contain',''+'Class '+classroom);
+            header.getClassName().should('contain',''+'Class '+qaClass);
         });
         it('will verify if group name is present', function(){
-            workspace.getGroupName().should('contain','Group'+' ');
+            teacherDashboard.getGroupName().should('contain',qaGroup).click();
+            teacherDashboard.joinGroup();
+
+            header.getGroupName().should('contain','Group '+qaGroup);
         });
         it('will verify group members is correct', function(){
-            workspace.getGroupMembers().each(($member,index, $list)=> {
-                expect(['S15', 'S16', 'S17', 'S18']).to.include($member.text());
+            header.getGroupMembers().each(($member,index, $list)=> {
+                expect(['S'+studentArr[0],'S'+studentArr[1],'S'+studentArr[2],'S'+studentArr[3]]).to.include($member.text());
             });
         });
         it('will verify student name is correct', function(){
-            workspace.getUserName().should('contain','Teacher '+teacher);
+            header.getUserName().should('contain','Teacher '+teacher);
         });
     });
 
-    describe('Check workspace comes up correctly', function(){
-        it('will verify left nav comes up correctly', function(){
-
-        });
-        it('will verify right nav comes up correctly', function(){
-
-        });
-        it('will verify bottom nav comes up correctly', function(){
-
-        });
+    describe('Check right nav for correctness', function(){
+       it('will verify that Class Work tab comes up and My Work tab is not visible', function(){
+           rightNav.getClassWorkTab().should('be.visible');
+           rightNav.getMyWorkTab().should('not.be.visible');
+       })
     });
 
-    describe('Check the student canvases restore correctly', function(){
+    describe('Check the student canvases restore correctly from Class Work', function(){
 
     });
 });
